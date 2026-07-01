dc97c035c82e77e6f183891a3cd167963016d105
Leonardo Giuseppe Ziche <lziche@gmail.com>
2026-07-01 14:39:49 +0200

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable: int = 10
+print(type(variable))
+variable = 10.2
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Hello"
+print(type(variable))
+variable = [2,3,4]
+print(type(variable))
