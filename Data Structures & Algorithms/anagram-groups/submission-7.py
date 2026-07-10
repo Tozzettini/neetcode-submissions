23a92936ee031aee9af22e8e84dfcc707c955dc9
Leonardo Giuseppe Ziche <lziche@gmail.com>
2026-07-10 11:09:15 +0200

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dic = {}
+
+        for i in strs:
+            key = "".join(sorted(i))
+            
+            if key in dic:
+              dic[key].append(i)
+            else:
+                dic[key] = []
+                dic[key].append(i)
+
+        
+            
+
+
+        return list(dic.values())
